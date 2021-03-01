2f78a999a3db7823048db16b528c460ef54563a0
maria lalij <marialalij@gmail.com>
2021-03-01 02:41:18 +0100

    admin roles

--- templates/administration.php
+++ templates/administration.php
@@ -1,66 +1,96 @@
-
 <?php $this->title = 'Administration'; ?>
+<link href="../public/admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <!-- Custom styles for this template-->
+    <link href="../public/admin/css/sb-admin-2.css" rel="stylesheet">
 
-<h1>Mon blog</h1>
-<p>En construction</p>
-<h2>Articles</h2>
+    <!-- Custom css -->
+
+    <!-- jquery -->
+    <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+
+</head>
+<h2> Mon espace administration</h2>
+<h3> Mes articles</h3>
+
 <?= $this->session->show('add_article'); ?>
 <?= $this->session->show('edit_article'); ?>
 <?= $this->session->show('delete_article'); ?>
 <?= $this->session->show('delete_comment'); ?>
 <?= $this->session->show('delete_user'); ?>
-<table>
-    <tr>
-        <td>Id</td>
-        <td>Titre</td>
-        <td>Contenu</td>
-        <td>Auteur</td>
-        <td>Date</td>
-        <td>Actions</td>
-    </tr>
-    <?php
-    foreach ($articles as $article)
-    {
-        ?>
-        <tr>
-            <td><?= htmlspecialchars($article->getIdArticle());?></td>
-            <td><a href="../public/index.php?route=article&articleId=<?= htmlspecialchars($article->getIdArticle());?>"><?= htmlspecialchars($article->getTitle());?></a></td>
-            <td><?= substr(htmlspecialchars($article->getContent()), 0, 150);?></td>
-            <td><?= htmlspecialchars($article->getAuthor());?></td>
-            <td>Créé le : <?= htmlspecialchars($article->getUpdateDate());?></td>
-            <td>
-                <a href="../public/index.php?route=editArticle&articleId=<?= $article->getIdArticle(); ?>">Modifier</a>
-                <a href="../public/index.php?route=deleteArticle&articleId=<?= $article->getIdArticle(); ?>">Supprimer</a>
-            </td>
-        </tr>
-        <?php
-    }
-    ?>
-</table>
 
-
-
-<h2>Utilisateurs</h2>
-<table>
-    <tr>
-        <td>Id</td>
-        <td>Pseudo</td>
-        <td>Rôle</td>
-        <td>Actions</td>
-    </tr>
+<div class="collapse show" id="recentPostsCard">
+                <div class="card-body">
+                    <table class="table table-hover table-responsive">
+                        <thead>
+                         <tr>
+                            <th scope="col" class="responsive-table-custom">Id</th>
+                            <th scope="col">Titre</th>
+                            <th scope="col">chapo</th>
+                            <th scope="col">Contenu</th>
+                            <th scope="col">Auteur</th>
+                            <th scope="col" class="responsive-table-custom">Date de création</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                 foreach ($articles as $article)
+                                 {
+                              ?>
+                                 <th scope="row"  class="responsive-table-custom"><?= htmlspecialchars($article->getIdArticle(), ENT_QUOTES); ?></th>
+                                <td><?= htmlspecialchars($article->getTitle(), ENT_QUOTES); ?></td>
+                                <td><?= htmlspecialchars($article->getChapo(), ENT_QUOTES); ?></td>
+                                <td><?= substr(htmlspecialchars($article->getContent(), ENT_QUOTES), 0, 50); ?>...</td>
+                                <td><?= htmlspecialchars($article->getAuthor(), ENT_QUOTES); ?></td>
+                                <td class="responsive-table-custom"><?= $article->getUpdateDate(); ?></td>
+                                <td>
+                                
+                           
+                                    <a href="../public/index.php?route=editArticle&articleId<?= $article->getIdArticle() ?>" class="btn btn-outline-dark btn-sm" title="Modifier">
+                                        <i class="fas fa-pencil-alt"></i>
+                                    </a>
+                                    <a href="../public/index.php?route=deleteArticle&articleId=<?= $article->getIdArticle(); ?>" class="btn btn-outline-dark btn-sm" title="Supprimer">
+                                        <i class="fas fa-trash-alt"></i>
+            
+                                    </a>
+                                  
+                                </td>
+                           
+                        </tbody>
+                        <?php
+                            }
+                            ?>
+                    </table>
+                </div>
+            </div>
+<h3>Utilisateurs</h3>
+<div class="collapse show" id="recentPostsCard">
+                <div class="card-body">
+                    <table class="table table-hover table-responsive">
+                        <thead>
+                         <tr>
+                            <th scope="col" class="responsive-table-custom">Id</th>
+                            <th scope="col">pseudo</th>
+                            <th scope="col">role</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
     <?php
     foreach ($users as $user)
     {
         ?>
-        <tr>
-            <td><?= htmlspecialchars($user->getIdUser());?></td>
-            <td><?= htmlspecialchars($user->getPseudo());?></td>
-            <td><?= htmlspecialchars($user->getRole());?></td>
-            <td>
+         <th scope="row"  class="responsive-table-custom"><?= htmlspecialchars($user->getIdUser(), ENT_QUOTES); ?></th>
+        <td><?= htmlspecialchars($user->getPseudo(), ENT_QUOTES); ?></td>
+        <td><?= htmlspecialchars($user->getRole(), ENT_QUOTES); ?></td>
+        <td>
+           
                 <?php
                 if($user->getRole() != 'admin') {
                 ?>
-                <a href="../public/index.php?route=deleteUser&userId=<?= $user->getIdUser(); ?>">Supprimer</a>
+                <a href="../public/index.php?route=deleteUser&userId=<?= $user->getIdUser(); ?>" class="btn btn-outline-dark btn-sm" title="Supprimer">
+                             <i class="fas fa-trash-alt"></i>
                 <?php }
                 else {
                     ?>
@@ -68,37 +98,65 @@
                 <?php
                 }
                 ?>
-            </td>
-        </tr>
-        <?php
-    }
-    ?>
-
-</table>
-
+            </td>          
+                           
+            </tbody>
+            <?php
+             }
+             ?>
+            </table>
+          </div>
+         </div>
 
 <h2>Commentaires</h2>
-<table>
-    <tr>
-        <td>Id</td>
-        <td>Message</td>
-        <td>Date</td>
-        <td>Actions</td>
-    </tr>
+<div class="collapse show" id="recentPostsCard">
+                <div class="card-body">
+                    <table class="table table-hover table-responsive">
+                        <thead>
+                         <tr>
+                            <th scope="col" class="responsive-table-custom">Id</th>
+                            <th scope="col">message</th>
+                            <th scope="col" class="responsive-table-custom">Date de création</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
     <?php
     foreach ($comments as $comment)
     {
         ?>
-        <tr>
-            <td><?= htmlspecialchars($comment->getIdComment());?></td>
-            <td><?= substr(htmlspecialchars($comment->getContent()), 0, 150);?></td>
-            <td>Créé le : <?= htmlspecialchars($comment->getCommentDate());?></td>
-            <td>
-                <a href="../public/index.php?route=deleteComment&commentId=<?= $comment->getIdComment(); ?>">Supprimer le commentaire</a>
-            </td>
-        </tr>
-        <?php
-    }
-    ?>
-</table>
+       <th scope="row"  class="responsive-table-custom"><?= htmlspecialchars($comment->getIdComment(), ENT_QUOTES); ?></th>
+        <td><?= htmlspecialchars($comment->getContent(), ENT_QUOTES); ?></td>
+        <td><?= htmlspecialchars($comment->getCommentDate(), ENT_QUOTES); ?></td>
+        <td>
+            
+                
+                <a href="../public/index.php?route=deleteComment&commentId=<?= $comment->getIdComment(); ?>" class="btn btn-outline-dark btn-sm" title="Supprimer">
+                             <i class="fas fa-trash-alt"></i>
+         </td>
+         </tbody>
+                        <?php
+                            }
+                            ?>
+                    </table>
+                </div>
+            </div>
 
+<script src="../../public/admin/vendor/jquery/jquery.min.js"></script>
+    <script src="../public/admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="../public/admin/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="../public/admin/js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="../public/admin/vendor/chart.js/Chart.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="../public/admin/js/demo/chart-area-demo.js"></script>
+    <script src="../public/admin/js/demo/chart-pie-demo.js"></script>
+
+    <!-- Custom JS -->
+    <!--<script type="text/javascript" src="public/js/customjs.js" rel="stylesheet" />"></script>-->
