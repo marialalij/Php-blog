a588b87fe3409acb18ab01d58108ab6e687e7a05
maria lalij <marialalij@gmail.com>
2021-03-01 02:43:31 +0100

    page article

--- templates/form_article.php
+++ templates/form_article.php
@@ -1,31 +1,45 @@
 <?php
-/**
- * Si existance d'un article, alors on affiche ses données pré-remplies => modification
- * Sinon champs vide => insertion
- * Affichage des erreurs de validation des données sous les champs correspondants
- *
- * La route générée est :
- * Si données POST : edition donc editArticle pour modification
- * Si pas de données POST : addArticle pour ajout
- */
 
 $route = isset($post) && $post->get('idarticle') ? 'editArticle&articleId=' . $post->get('idarticle') : 'addArticle';
 $title = isset($post) ? htmlspecialchars($post->get('title')) : '';
 $chapo = isset($post) ? htmlspecialchars($post->get('chapo')) : '';
 $content = isset($post) ? htmlspecialchars($post->get('content')) : '';
+$author = isset($post) ? htmlspecialchars($post->get('content')) : '';
 $submit = $route === 'addArticle' ? 'Envoyer' : 'Mettre à jour';
 ?>
-<form action="../public/index.php?route=<?= $route; ?>" method="post">
-    <label for="title">Titre</label><br>
-    <input type="text" id="title" name="title" value="<?= $title; ?>"><br>
-    <?= isset($errors['title']) ? $errors['title'] : '' ?>
-
-    <label for="chapo">chapo</label><br>
-    <input type="text" id="chapo" name="chapo" value="<?= $chapo; ?>"><br>
-    <?= isset($errors['chapo']) ? $errors['chapo'] : '' ?>
-
-    <label for="content">Contenu</label><br>
-    <textarea name="content" id="contenu" cols="30" rows="10"><?= $content; ?></textarea><br>
-    <?= isset($errors['content']) ? $errors['content'] : '' ?>
-    <input type="submit" value="<?= $submit; ?>" id="submit" name="submit">
-</form>
+<div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-10 mx-auto">
+        <form action="../public/index.php?route=<?= $route; ?>" method="post">
+        <div class="control-group">
+        <div class="form-group floating-label-form-group controls">
+        <label for="title">Titre</label><br>
+          <input type="text" id="title" name="title"  class="form-control" placeholder="title"  required data-validation-required-message="Please enter your title."
+          value="<?= isset($post) ? htmlspecialchars($post->get('title')): ''; ?>"><br>
+          <?= isset($errors['title']) ? $errors['title'] : ''; ?>
+          <p class="help-block text-danger"></p>
+        </div>
+       </div>
+       <div class="control-group">
+        <div class="form-group floating-label-form-group controls">
+       
+          <label for="chapo">chapo</label><br>
+          <input type="text" id="chapo" name="chapo" class="form-control" placeholder="Chapo"  required data-validation-required-message="Please enter your chapo."
+          value="<?= isset($post) ? htmlspecialchars($post->get('chapo')): ''; ?>"><br>
+          <?= isset($errors['chapo']) ? $errors['chapo'] : ''; ?>
+          <p class="help-block text-danger"></p>
+          </div>   
+          </div>
+          <div class="control-group">
+        <div class="form-group floating-label-form-group controls">
+          <label for="content">content</label><br>
+          <textarea id="content" name="content" rows="10" class="form-control" placeholder="Content" required data-validation-required-message="Please enter a message.">
+          <?= isset($post) ? htmlspecialchars($post->get('content')): ''; ?></textarea><br>
+          <?= isset($errors['content']) ? $errors['content'] : ''; ?>
+          </div>
+          </div>
+         <input type="submit" value="<?= $submit; ?>" id="submit" class="btn btn-primary" name="submit">
+        </form>
+       </div>  
+    </div>
+</div>
