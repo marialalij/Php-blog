e37ed3e6398da4083964a9e53030ade534ed0fd4
maria lalij <marialalij@gmail.com>
2021-03-02 02:19:10 +0100

    admin comment config

--- src/model/CommentManager.php
+++ src/model/CommentManager.php
@@ -11,13 +11,14 @@ private function buildObject($row)
         $comment->setIdComment($row['idcomment']);
         $comment->setContent($row['content']);
         $comment->setCommentDate($row['comment_date']);
+        $comment->setStatus($row['status']);
         return $comment;
     }
 
-    public function getComments($postId)
+    public function getComments($articleId)
     {
-        $sql = 'SELECT idcomment, content, comment_date FROM comment WHERE article_idarticle= ? ORDER BY comment_date DESC';
-        $result = $this->createQuery($sql, [$postId]);
+        $sql = 'SELECT idcomment, content, comment_date, status FROM comment WHERE article_idarticle= ? and status = 1 ORDER BY comment_date DESC';
+        $result = $this->createQuery($sql, [$articleId]);
         $comments = [];
         foreach ($result as $row) {
             $commentId = $row['idcomment'];
@@ -30,7 +31,7 @@ private function buildObject($row)
 
     public function addComment(Parameter $post, $articleId)
     {
-        $sql = 'INSERT INTO comment (content, comment_date, article_idarticle) VALUES (?, NOW(), ?)';
+        $sql = 'INSERT INTO comment (content, comment_date, article_idarticle, status) VALUES (?, NOW(), ?,2)';
         $this->createQuery($sql, [$post->get('content'), $articleId]);
     }
 
@@ -45,7 +46,7 @@ private function buildObject($row)
 
     public function getAllComments()
     {
-        $sql = 'SELECT idcomment,  content, comment_date FROM comment ORDER BY comment_date DESC';
+        $sql = 'SELECT idcomment, content, comment_date, status FROM comment ORDER BY comment_date DESC';
         $result = $this->createQuery($sql);
         $comments = [];
         foreach ($result as $row) {
@@ -55,6 +56,22 @@ private function buildObject($row)
         $result->closeCursor();
         return $comments;
     
+}
+
+ 
+public function publishComment($commentId)
+{
+   $sql = 'UPDATE comment SET status = 1 WHERE idcomment = ?';
+   $this->createQuery($sql,[$commentId]);
+
+}
+
+public function pauseComment($commentId)
+{
+ $sql = 'UPDATE comment SET status = 2 WHERE idcomment = ?';
+ $this->createQuery($sql, [$commentId]);
+
+ 
 }
 
 }
